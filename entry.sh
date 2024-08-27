f8e2525ab4cdc30efde29a795f81340d426d87c5
LordOdin <derekrb25017@gmail.com>
2024-08-26 21:42:22 -0400

    Cleanup

--- entry.sh
+++ entry.sh
@@ -4,30 +4,34 @@
 if [ ! "$(id -u vintagestory)" -eq "$UID" ]; then usermod -o -u "$UID" vintagestory ; fi
 if [ ! "$(id -g vintagestory)" -eq "$GID" ]; then groupmod -o -g "$GID" vintagestory ; fi
 
-# Update server
 
 cd /data
-wget https://cdn.vintagestory.at/gamefiles/$SERVER_BRANCH/vs_server_linux-x64_$SERVER_VERSION.tar.gz
-tar xzf vs_server_linux-x64_*.tar.gz
-rm vs_server_linux-x64_*.tar.gz
-echo "$SERVER_VERSION" > .version
 chown -R vintagestory:vintagestory /data
 
-# Apply server configuration
-serverconfig="/data/server-file/serverconfig.json"
+# Update server
+if [ ! -f /data/vs_server_linux-x64_$SERVER_VERSION.tar.gz ]; then
+  wget https://cdn.vintagestory.at/gamefiles/$SERVER_BRANCH/vs_server_linux-x64_$SERVER_VERSION.tar.gz
+  tar xzf vs_server_linux-x64_*.tar.gz
+fi
 
-if [ ! -f serverconfig ]; then
+
+# Copy Default serverconfig.json
+if [ ! -f /data/server-file/serverconfig.json ]; then
 cp /tmp/default-serverconfig.json /data/server-file/serverconfig.json
 fi
 
-#make mods folder
+
+# Make Mods directory
 if [ ! -d /data/server-file/Mods ]; then
   mkdir /data/server-file/Mods
 fi
 
+
+# Move mods to Mods directory
 if [ -d /data/server-file/Mods ]; then
   mv -f /tmp/mods/* /data/server-file/Mods/
 fi
+
 
 # Start server
 echo "Launching server..."
